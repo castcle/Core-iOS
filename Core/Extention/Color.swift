fb05e7a616eeeacdf9f96ef80c4b926bcec3906b
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-07 16:07:46 +0700

    Add Assets to project.

---
+++ Core/Extention/Color.swift
@@ -0,0 +1,54 @@
+//
+//  Color.swift
+//  Core
+//
+//  Created by Tanakorn Phoochaliaw on 7/7/2564 BE.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    struct Asset {
+        public static let black = UIColor(named: "Black", in: ConfigBundle.core, compatibleWith: nil)!
+        public static let darkGraphiteBlue = UIColor(named: "DarkGraphiteBlue", in: ConfigBundle.core, compatibleWith: nil)!
+        public static let darkGray = UIColor(named: "DarkGray", in: ConfigBundle.core, compatibleWith: nil)!
+        public static let lightBlue = UIColor(named: "LightBlue", in: ConfigBundle.core, compatibleWith: nil)!
+        public static let lightGray = UIColor(named: "LightGray", in: ConfigBundle.core, compatibleWith: nil)!
+        public static let white = UIColor(named: "White", in: ConfigBundle.core, compatibleWith: nil)!
+    }
+}
+
+public extension UIColor {
+    convenience init?(hexString: String) {
+        
+        if hexString.hasPrefix("#") {
+            var hexColor: String = hexString.replacingOccurrences(of: "#", with: "")
+            if hexColor.count == 6 {
+                hexColor = "\(hexColor)FF"
+            }
+            
+            if hexColor.count == 8 {
+                let scanner: Scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    let red: UInt64 = ((hexNumber & 0xff000000) >> 24)
+                    let green: UInt64 = ((hexNumber & 0x00ff0000) >> 16)
+                    let blue: UInt64 = ((hexNumber & 0x0000ff00) >> 8)
+                    let alpha: UInt64 = (hexNumber & 0x000000ff)
+                    
+                    self.init(
+                        red: CGFloat(red) / 255.0,
+                        green: CGFloat(green) / 255.0,
+                        blue: CGFloat(blue) / 255.0,
+                        alpha: CGFloat(alpha) / 255.0)
+                    
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+}
---
+++ Core/Extention/Font.swift
@@ -0,0 +1,95 @@
+//
+//  Font.swift
+//  Core
+//
+//  Created by Tanakorn Phoochaliaw on 7/7/2564 BE.
+//
+
+import UIKit
+
+public extension UIFont {
+    
+    // load framework font in application
+    static let loadAllFonts: () = {
+        registerFont(withFilenameString: "Kanit-Medium.ttf", bundle: ConfigBundle.core)
+        registerFont(withFilenameString: "Sarabun-Light.ttf", bundle: ConfigBundle.core)
+        registerFont(withFilenameString: "Sarabun-Regular.ttf", bundle: ConfigBundle.core)
+    }()
+
+    //MARK: - Make custom font bundle register to framework
+    static func registerFont(withFilenameString filenameString: String, bundle: Bundle) {
+
+        guard let pathForResourceString = bundle.path(forResource: filenameString, ofType: nil) else {
+            print("UIFont+:  Failed to register font - path for resource not found.")
+            return
+        }
+
+        guard let fontData = NSData(contentsOfFile: pathForResourceString) else {
+            print("UIFont+:  Failed to register font - font data could not be loaded.")
+            return
+        }
+
+        guard let dataProvider = CGDataProvider(data: fontData) else {
+            print("UIFont+:  Failed to register font - data provider could not be loaded.")
+            return
+        }
+
+        guard let font = CGFont(dataProvider) else {
+            print("UIFont+:  Failed to register font - font could not be loaded.")
+            return
+        }
+
+        var errorRef: Unmanaged<CFError>? = nil
+        if (CTFontManagerRegisterGraphicsFont(font, &errorRef) == false) {
+            print("UIFont+:  Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
+        }
+    }
+    
+    enum Fonts: String, CaseIterable {
+        case medium = "Kanit-Medium"
+        case light = "Sarabun-Light"
+        case regular = "Sarabun-Regular"
+    }
+
+    enum FontSize {
+        case title
+        case h1
+        case h2
+        case h3
+        case h4
+        case h5
+        case body
+        case overline
+        case small
+        case custom(size: CGFloat)
+
+        var value: CGFloat {
+            switch self {
+            case .title:
+                return 30
+            case .h1:
+                return 30
+            case .h2:
+                return 24
+            case .h3:
+                return 20
+            case .h4:
+                return 18
+            case .h5:
+                return 16
+            case .body:
+                return 16
+            case .overline:
+                return 14
+            case .small:
+                return 12
+            case .custom(let size):
+                return size
+            }
+        }
+    }
+
+    static func asset(_ font: Fonts, fontSize: FontSize) -> UIFont {
+        return UIFont(name: font.rawValue, size: fontSize.value)!
+    }
+}
