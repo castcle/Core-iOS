bd44ec9444ad2004e5898cde33aa004147a24629
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-08-18 16:02:51 +0700

    Add page data

--- Core/Helper/UserState.swift
+++ Core/Helper/UserState.swift
@@ -27,6 +27,16 @@
 
 import Defaults
 
+public struct Page {
+    public var name: String = ""
+    public var avatar: String = ""
+    
+    public init(name: String, avatar: String) {
+        self.name = name
+        self.avatar = avatar
+    }
+}
+
 public class UserState: NSObject {
     public static let shared = UserState()
     
@@ -54,6 +64,15 @@ public class UserState: NSObject {
     
     public var cover: String {
         return "https://cdn.pixabay.com/photo/2020/02/11/16/25/manarola-4840080_1280.jpg"
+    }
+    
+    public var page: [Page] {
+        return [
+            Page(name: "Manchester United", avatar: "https://seeklogo.com/images/M/manchester-united-logo-F14DA1FCCD-seeklogo.com.png"),
+            Page(name: "Manchester City", avatar: "https://upload.wikimedia.org/wikipedia/th/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png"),
+            Page(name: "Chelsea FC", avatar: "https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png"),
+            Page(name: "Liverpool FC", avatar: "https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515361_10542.jpg")
+        ]
     }
     
     public func login() {
