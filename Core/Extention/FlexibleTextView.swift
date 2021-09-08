dd7fb610101548a1ef1996c067f2d70eaf54d14c
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-09-08 21:16:38 +0700

    Add extension TextView

---
+++ Core/Extention/FlexibleTextView.swift
@@ -0,0 +1,119 @@
+//  Copyright (c) 2021, Castcle and/or its affiliates. All rights reserved.
+//  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+//  This code is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License version 3 only, as
+//  published by the Free Software Foundation.
+//
+//  This code is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+//  version 3 for more details (a copy is included in the LICENSE file that
+//  accompanied this code).
+//
+//  You should have received a copy of the GNU General Public License version
+//  3 along with this work; if not, write to the Free Software Foundation,
+//  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//  Please contact Castcle, 22 Phet Kasem 47/2 Alley, Bang Khae, Bangkok,
+//  Thailand 10160, or visit www.castcle.com if you need additional information
+//  or have any questions.
+//
+//  FlexibleTextView.swift
+//  Core
+//
+//  Created by Tanakorn Phoochaliaw on 7/9/2564 BE.
+//
+
+import Foundation
+import UIKit
+
+public class FlexibleTextView: UITextView {
+    // limit the height of expansion per intrinsicContentSize
+    public var maxHeight: CGFloat = 0.0
+    private let placeholderTextView: UITextView = {
+        let tv = UITextView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = .clear
+        tv.isScrollEnabled = false
+        tv.isUserInteractionEnabled = false
+        tv.textColor = UIColor.gray
+        return tv
+    }()
+    public var placeholder: String? {
+        get {
+            return placeholderTextView.text
+        }
+        set {
+            placeholderTextView.text = newValue
+        }
+    }
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        isScrollEnabled = false
+        autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        NotificationCenter.default.addObserver(self, selector: #selector(UITextInputDelegate.textDidChange(_:)), name: UITextView.textDidChangeNotification, object: self)
+        placeholderTextView.font = font
+        addSubview(placeholderTextView)
+        
+        NSLayoutConstraint.activate([
+            placeholderTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            placeholderTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 5),
+            placeholderTextView.topAnchor.constraint(equalTo: topAnchor),
+            placeholderTextView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override var text: String! {
+        didSet {
+            invalidateIntrinsicContentSize()
+            placeholderTextView.isHidden = !text.isEmpty
+        }
+    }
+    
+    public override var font: UIFont? {
+        didSet {
+            placeholderTextView.font = font
+            invalidateIntrinsicContentSize()
+        }
+    }
+    
+    public override var contentInset: UIEdgeInsets {
+        didSet {
+            placeholderTextView.contentInset = contentInset
+        }
+    }
+    
+    public override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        
+        if size.height == UIView.noIntrinsicMetric {
+            // force layout
+            layoutManager.glyphRange(for: textContainer)
+            size.height = layoutManager.usedRect(for: textContainer).height + textContainerInset.top + textContainerInset.bottom
+        }
+        
+        if maxHeight > 0.0 && size.height > maxHeight {
+            size.height = maxHeight
+            
+            if !isScrollEnabled {
+                isScrollEnabled = true
+            }
+        } else if isScrollEnabled {
+            isScrollEnabled = false
+        }
+        
+        return size
+    }
+    
+    @objc private func textDidChange(_ note: Notification) {
+        // needed incase isScrollEnabled is set to true which stops automatically calling invalidateIntrinsicContentSize()
+        invalidateIntrinsicContentSize()
+        placeholderTextView.isHidden = !text.isEmpty
+    }
+}
