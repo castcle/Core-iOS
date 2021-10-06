df5292932e5bf2f655aed9cbe1f7b2cb4cbb2045
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-10-06 11:36:55 +0700

    Add detect link from string

---
+++ Core/Helper/DataDetector.swift
@@ -0,0 +1,75 @@
+//  Copyright (c) 2021, Castcle and/or its affiliates. All rights reserved.
+//  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+//  This code is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License version 3 only, as
+//  published by the Free Software Foundation.
+//
+//  This code is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+//  version 3 for more details (a copy is included in the LICENSE file that
+//  accompanied this code).
+//
+//  You should have received a copy of the GNU General Public License version
+//  3 along with this work; if not, write to the Free Software Foundation,
+//  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//  Please contact Castcle, 22 Phet Kasem 47/2 Alley, Bang Khae, Bangkok,
+//  Thailand 10160, or visit www.castcle.com if you need additional information
+//  or have any questions.
+//
+//  DataDetector.swift
+//  Core
+//
+//  Created by Tanakorn Phoochaliaw on 6/10/2564 BE.
+//
+
+// MARK: DataDetector
+
+class DataDetector {
+    private class func _find(all type: NSTextCheckingResult.CheckingType,
+                             in string: String, iterationClosure: (String) -> Bool) {
+        guard let detector = try? NSDataDetector(types: type.rawValue) else { return }
+        let range = NSRange(string.startIndex ..< string.endIndex, in: string)
+        let matches = detector.matches(in: string, options: [], range: range)
+        loop: for match in matches {
+            for i in 0 ..< match.numberOfRanges {
+                let nsrange = match.range(at: i)
+                let startIndex = string.index(string.startIndex, offsetBy: nsrange.lowerBound)
+                let endIndex = string.index(string.startIndex, offsetBy: nsrange.upperBound)
+                let range = startIndex..<endIndex
+                guard iterationClosure(String(string[range])) else { break loop }
+            }
+        }
+    }
+
+    class func find(all type: NSTextCheckingResult.CheckingType, in string: String) -> [String] {
+        var results = [String]()
+        _find(all: type, in: string) {
+            results.append($0)
+            return true
+        }
+        return results
+    }
+
+    class func first(type: NSTextCheckingResult.CheckingType, in string: String) -> String? {
+        var result: String?
+        _find(all: type, in: string) {
+            result = $0
+            return false
+        }
+        return result
+    }
+}
+
+// MARK: String extension
+public extension String {
+    var detectedLinks: [String] { DataDetector.find(all: .link, in: self) }
+    var detectedFirstLink: String? { DataDetector.first(type: .link, in: self) }
+    var detectedURLs: [URL] { detectedLinks.compactMap { URL(string: $0) } }
+    var detectedFirstURL: URL? {
+        guard let urlString = detectedFirstLink else { return nil }
+        return URL(string: urlString)
+    }
+}
